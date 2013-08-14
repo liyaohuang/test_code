e5228f916faaa3fe76eb25a45af454921cce6563
rongji.li <rongji.li@b5m.com>
2013-08-14 16:46:21 +0800

    my_branch_modify1

--- src/com/lee/btrace/QuerySqlTrace.java
+++ src/com/lee/btrace/QuerySqlTrace.java
@@ -8,7 +8,7 @@ import com.sun.btrace.annotations.*;
 
 import static com.sun.btrace.BTraceUtils.*;
 
-//统计hibernate下的一些sql查询执行情况
+//统计hibernate下的一些sql查询执行情况   分支下修改
 @BTrace
 public class QuerySqlTrace {
 	private static long execStatementCount = 0l;  
